af46832cadd9070c32645e41ad340591492424d2
Dima <dmitry.usov@yahoo.com>
2024-05-08 23:54:27 +0300

    chore: R20-29 Install commercetools SDK (#11)
    * chore: add sdk-client-v2 and platform-sdk
    
    * chore: add .env.example
    
    * docs: add info about .env
    
    * chore: add polyfill plugins
    
    * fix: update vite.config.ts to polyfill
    
    * feat: add apiRoot

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,37 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
+import rollupNodePolyFill from 'rollup-plugin-node-polyfills';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
   plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      stream:
+        'rollup-plugin-node-polyfills/polyfills/stream',
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: 'globalThis',
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          process: true,
+          buffer: true,
+        }),
+        NodeModulesPolyfillPlugin(),
+      ],
+    },
+  },
+  build: {
+    rollupOptions: {
+      plugins: [rollupNodePolyFill()],
+    },
+  },
 });
