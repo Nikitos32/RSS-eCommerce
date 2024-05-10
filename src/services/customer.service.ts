86ec7606a4bf2bf655e3291953c2689b2cbb1b01
Dima Usov <dmitry.usov@yahoo.com>
2024-05-10 08:41:39 +0300

    feat: add createDraftFromRegForm

--- src/services/customer.service.ts
+++ src/services/customer.service.ts
@@ -1,4 +1,5 @@
 import {
+  BaseAddress,
   ClientResponse,
   CustomerDraft,
   CustomerSignInResult,
@@ -45,5 +46,100 @@ export class CustomerService {
         error as ClientResponse
       );
     }
+  }
+
+  createDraft(
+    email: string,
+    password: string,
+    firstName?: string,
+    lastName?: string,
+    dateOfBirth?: string,
+    addresses?: BaseAddress[],
+    shippingAddresses?: number[],
+    defaultShippingAddress?: number,
+    billingAddresses?: number[],
+    defaultBillingAddress?: number
+  ): CustomerDraft {
+    const customerDraft: CustomerDraft =
+      {
+        email,
+        password,
+        firstName,
+        lastName,
+        dateOfBirth,
+        addresses,
+        defaultShippingAddress,
+        shippingAddresses,
+        defaultBillingAddress,
+        billingAddresses,
+      };
+    return customerDraft;
+  }
+
+  createAddress(
+    country: string,
+    firstName?: string,
+    lastName?: string,
+    streetName?: string,
+    city?: string,
+    postalCode?: string
+  ): BaseAddress {
+    const address: BaseAddress = {
+      country,
+      firstName,
+      lastName,
+      streetName,
+      city,
+      postalCode,
+    };
+    return address;
+  }
+
+  createDraftFromRegForm(
+    email: string,
+    password: string,
+    firstName: string,
+    lastName: string,
+    dateOfBirth: string,
+    streetName: string,
+    city: string,
+    postalCode: string,
+    country: string,
+    isDefaultAddress: boolean
+  ): CustomerDraft {
+    const address = this.createAddress(
+      country,
+      lastName,
+      firstName,
+      streetName,
+      city,
+      postalCode
+    );
+
+    const shippingAddresses =
+      isDefaultAddress
+        ? [0]
+        : undefined;
+    const defaultShippingAddress =
+      isDefaultAddress ? 0 : undefined;
+    const billingAddresses =
+      isDefaultAddress
+        ? [0]
+        : undefined;
+    const defaultBillingAddress =
+      isDefaultAddress ? 0 : undefined;
+
+    return this.createDraft(
+      email,
+      password,
+      firstName,
+      lastName,
+      dateOfBirth,
+      [address],
+      shippingAddresses,
+      defaultShippingAddress,
+      billingAddresses,
+      defaultBillingAddress
+    );
   }
 }
