4a34afa165a5d3b089dbe15329e472a3022d9712
Dima <dmitry.usov@yahoo.com>
2024-05-23 11:34:25 +0300

    feat: R20-80 User Input Text (#37)
    * feat: add UserInputString
    
    * fix: various improvements
    
    * refactor: move makeIdFromLabel to separate file
    
    * fix: makeIdFromLabel empty input
    
    * test: makeIdFromLabel
    
    * fix: clue design

---
+++ src/components/UserInputString.tsx
@@ -0,0 +1,78 @@
+import { Dispatch, SetStateAction } from 'react';
+import { makeIdFromLabel } from '../utils';
+import { BsInfoCircle } from 'react-icons/bs';
+
+type UserInputStringProps = {
+  type: 'text' | 'password';
+  label?: string;
+  placeHolder?: string;
+  isRequired?: boolean;
+  autocomplete: 'on' | 'off';
+  valueUseState: string;
+  setValueUseState: Dispatch<SetStateAction<string>>;
+  isValidValueUseState: boolean;
+  isFocusUseState: boolean;
+  setFocusUseState: Dispatch<SetStateAction<boolean>>;
+  elementUseRef?: React.RefObject<HTMLInputElement>;
+  clues?: JSX.Element;
+};
+
+/**
+ * @description standard input component for user string fields
+ * @param props
+ * @returns
+ */
+function UserInputString(props: UserInputStringProps) {
+  const id = makeIdFromLabel(props.label || 'no label');
+  const cluesId = `clues-${id}`;
+
+  return (
+    <>
+      <label
+        className="block text-gray-700 text-sm font-bold mb-2"
+        htmlFor={id}
+      >
+        {props.label}
+      </label>
+      <input
+        className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+          !props.valueUseState || props.isValidValueUseState
+            ? ''
+            : ' border-red-500'
+        }`}
+        id={id}
+        type={props.type}
+        autoComplete="off"
+        onChange={(e) => props.setValueUseState(e.target.value)}
+        required={props.isRequired}
+        placeholder={props.placeHolder}
+        aria-invalid={props.isValidValueUseState ? 'false' : 'true'}
+        aria-describedby={cluesId}
+        onFocus={
+          props.setFocusUseState ? () => props.setFocusUseState(true) : () => {}
+        }
+        onBlur={
+          props.setFocusUseState
+            ? () => props.setFocusUseState(false)
+            : () => {}
+        }
+        ref={props.elementUseRef}
+      />
+      <div
+        id={cluesId}
+        className={`mt-1 flex flex-row rounded-md bg-moonNeutral-200 md:max-w-2xl md:mx-auto justify-start items-center gap-2 text-sm ${
+          props.clues &&
+          props.isFocusUseState &&
+          (!props.valueUseState || !props.isValidValueUseState)
+            ? ' '
+            : ' hidden'
+        }`}
+      >
+        <BsInfoCircle className="ml-2 text-xl" />
+        {props.clues}
+      </div>
+    </>
+  );
+}
+
+export default UserInputString;
