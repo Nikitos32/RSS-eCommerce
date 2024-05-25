86a645563a47d39f52c7bde9429eec3d84adc741
Dima Usov <dmitry.usov@yahoo.com>
2024-05-25 13:14:09 +0300

    test: UserInput name and email

--- src/utils/UserInput.test.ts
+++ src/utils/UserInput.test.ts
@@ -13,4 +13,29 @@ describe('UserInput', () => {
     assert.equal(UserInput.checkNameRequiredValid('Name'), true);
     assert.equal(UserInput.checkNameRequiredValid('name'), true);
   });
+
+  it('should return false for invalid name', () => {
+    assert.equal(UserInput.checkNameValid('3'), false);
+    assert.equal(UserInput.checkNameValid(' '), false);
+  });
+
+  it('should return true for valid name', () => {
+    assert.equal(UserInput.checkNameValid(''), true);
+    assert.equal(UserInput.checkNameValid('E'), true);
+    assert.equal(UserInput.checkNameValid('Name'), true);
+    assert.equal(UserInput.checkNameValid('name'), true);
+  });
+
+  it('should return false for invalid required email', () => {
+    assert.equal(UserInput.checkEmailRequiredValid(''), false);
+    assert.equal(UserInput.checkEmailRequiredValid('3'), false);
+    assert.equal(UserInput.checkEmailRequiredValid(' '), false);
+    assert.equal(UserInput.checkEmailRequiredValid('test@example'), false);
+    assert.equal(UserInput.checkEmailRequiredValid('test@example.c'), false);
+  });
+
+  it('should return true for valid required email', () => {
+    assert.equal(UserInput.checkEmailRequiredValid('test@example.com'), true);
+    assert.equal(UserInput.checkEmailRequiredValid('te.st@example.co'), true);
+  });
 });
