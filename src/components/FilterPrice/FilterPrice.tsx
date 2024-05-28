3c06e19856cd761f6673ee6ba3d326e6b46467f4
Nikita <nikita456nikita@yandex.ru>
2024-05-29 00:53:34 +0300

    feat: add visibility for the current state of range input

--- src/components/FilterPrice/FilterPrice.tsx
+++ src/components/FilterPrice/FilterPrice.tsx
@@ -1,10 +1,23 @@
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
+import { useState } from 'react';
 
 export const FilterPrice = () => {
+  const [currentRangeValue, setCurrentRangeValue] = useState<number[]>([
+    0, 1000,
+  ]);
+  const handleRangeSlider = (event: number | number[]) => {
+    if (typeof event !== 'number') {
+      setCurrentRangeValue(event);
+    }
+  };
+
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col gap-3 ">
       <p>Price:</p>
+      <p>
+        Current Range: ${currentRangeValue[0]} - ${currentRangeValue[1]}
+      </p>
       <div className="w-36">
         <Slider
           marks={{ 0: '$0', 1000: '$1000' }}
@@ -14,6 +27,7 @@ export const FilterPrice = () => {
           min={0}
           max={1000}
           pushable={100}
+          onChange={(event) => handleRangeSlider(event)}
         />
       </div>
     </div>
