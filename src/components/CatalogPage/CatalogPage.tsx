95420f16d47784f8cbcfead2f6ec80d13bfc7ade
Nikita <nikita456nikita@yandex.ru>
2024-05-29 02:27:19 +0300

    feat: add style and some icons

--- src/components/CatalogPage/CatalogPage.tsx
+++ src/components/CatalogPage/CatalogPage.tsx
@@ -1,4 +1,5 @@
 import { FilterSection } from '../FilterSection/FilterSection';
+import { ProductPreviewItem } from '../ProductPreviewItem/ProductPreviewItem';
 import { SortSection } from '../SortSection/SortSection';
 
 export const CatalogPage = () => {
@@ -7,8 +8,17 @@ export const CatalogPage = () => {
       <div>
         <FilterSection />
       </div>
-      <div className="flex flex-col w-full p-5">
+      <div className="flex gap-5 flex-col w-full p-5">
         <SortSection />
+        <div className="flex flex-col gap-3">
+          <h1 className="text-2xl">Products</h1>
+          <section className="flex flex-col gap-5 flex-wrap">
+            <ProductPreviewItem />
+            <ProductPreviewItem />
+            <ProductPreviewItem />
+            <ProductPreviewItem />
+          </section>
+        </div>
       </div>
     </section>
   );
