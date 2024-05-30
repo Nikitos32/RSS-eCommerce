2138ab1e5e67ac1872bf8663b9dd05800b4cba54
Dima Usov <dmitry.usov@yahoo.com>
2024-05-31 00:14:12 +0300

    feat:add hook useApiGetCustomer

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,4 @@
 export { useLocalStorage } from './useLocalStorage';
 export { useApiChangePassword } from './useApiChangePassword';
 export { useApiSignIn } from './useApiSignIn';
+export { useApiGetCustomer } from './useApiGetCustomer';
