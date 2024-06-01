f9c4b3ab3f2724028298f11af33ba574d53e4daf
Barvinko <74064627+Barvinko@users.noreply.github.com>
2024-06-01 14:26:44 +0300

    feat: 20-77 Implement Slider Modal (#49)
    * feat: add swiper of images to modal
    
    * feat: realise transfer of current picture to modal via Controller
    
    * feat: add synchronisation of swipers with swiper of modal
    
    * feat: add visual highlighting of selected picture
    
    * feat: add navigation to main swiper
    
    * feat: add more cursor types for modal

--- src/components/ProductPage/ProductPageSwiper/EnlargedImageModal/EnlargedImageModal.tsx
+++ src/components/ProductPage/ProductPageSwiper/EnlargedImageModal/EnlargedImageModal.tsx
@@ -1,16 +1,34 @@
 import { IoCloseCircle } from 'react-icons/io5';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import {
+  FreeMode,
+  Thumbs,
+  Mousewheel,
+  Pagination,
+  Controller,
+} from 'swiper/modules';
+import { Swiper as SwiperType } from 'swiper/types';
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/zoom';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 import './enlargedImageModal.css';
 
 type ProductSwiperProps = {
-  currentImage: string;
+  images: string[];
+  currentImage: SwiperType | null;
   flagDialog: boolean;
   onclick: () => void;
+  setCurrentImage: (arg0: SwiperType) => void;
 };
 
 export const EnlargedImageModal = ({
+  images,
   currentImage,
   flagDialog,
   onclick,
+  setCurrentImage,
 }: ProductSwiperProps) => {
   return (
     <dialog
@@ -27,7 +45,29 @@ export const EnlargedImageModal = ({
             className="ml-auto text-3xl text-moonBlack hover:text-black transition-colors cursor-pointer"
           />
         </div>
-        <img className="productDialog__img" src={currentImage} alt="product" />
+        <Swiper
+          modules={[FreeMode, Thumbs, Mousewheel, Pagination, Controller]}
+          controller={{ control: currentImage }}
+          onSwiper={setCurrentImage}
+          direction="vertical"
+          grabCursor={true}
+          mousewheel={true}
+          pagination={{
+            clickable: true,
+          }}
+          centeredSlides={true}
+          slidesPerView={1}
+          spaceBetween={10}
+          className="productDialog__swiper"
+        >
+          {images.map((img, index) => (
+            <SwiperSlide key={index}>
+              <div className="swiper-zoom-container">
+                <img className="object-contain h-full" src={img} />
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </dialog>
   );
--- src/components/ProductPage/ProductPageSwiper/ProductSwiper.tsx
+++ src/components/ProductPage/ProductPageSwiper/ProductSwiper.tsx
@@ -1,10 +1,11 @@
 import { useState } from 'react';
 import { EnlargedImageModal } from './EnlargedImageModal/EnlargedImageModal';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { FreeMode, Thumbs } from 'swiper/modules';
+import { FreeMode, Thumbs, Controller, Navigation } from 'swiper/modules';
 import { Swiper as SwiperType } from 'swiper/types';
 import 'swiper/css';
 import 'swiper/css/free-mode';
+import 'swiper/css/zoom';
 import 'swiper/css/navigation';
 import 'swiper/css/thumbs';
 import './productSwiper.css';
@@ -16,52 +17,60 @@ type ProductSwiperProps = {
 export const ProductSwiper = ({ images }: ProductSwiperProps) => {
   const [thumbsSwiper, setThumbsSwiper] = useState<SwiperType | null>(null);
   const [flagDialog, setFlagDialog] = useState(false);
-  const [currentImage, setCurrentImage] = useState(images[0]);
+  const [dialogImage, setCurrentImage] = useState<SwiperType | null>(null);
+  const [mainSlide, setMainSlide] = useState<SwiperType | null>(null);
 
   return (
     <>
       <div>
         <Swiper
-          onClick={() => setFlagDialog(true)}
-          onTouchEnd={() => setFlagDialog(true)}
+          onClick={() => {
+            setFlagDialog(true);
+          }}
+          navigation={true}
+          controller={{ control: mainSlide }}
+          onSwiper={setCurrentImage}
           thumbs={{ swiper: thumbsSwiper }}
-          modules={[FreeMode, Thumbs]}
+          modules={[FreeMode, Thumbs, Controller, Navigation]}
           allowTouchMove={false}
           centeredSlides={true}
           className="productImg__main-swiper"
         >
           {images.map((img, index) => (
             <SwiperSlide className="productImg__slide" key={index}>
-              <img
-                src={img}
-                onClick={() => {
-                  setCurrentImage(img);
-                }}
-              />
+              <img src={img} />
             </SwiperSlide>
           ))}
         </Swiper>
         <Swiper
           onSwiper={setThumbsSwiper}
+          grabCursor={true}
           slidesPerView={5}
+          spaceBetween={5}
           freeMode={true}
+          watchSlidesProgress={true}
           centerInsufficientSlides={true}
-          modules={[FreeMode, Thumbs]}
+          modules={[FreeMode, Thumbs, Navigation]}
           className="productImg__footer-swiper"
         >
           {images.length > 1
             ? images.map((img, index) => (
-                <SwiperSlide className="productImg__slide" key={index}>
-                  <img src={img} />
+                <SwiperSlide
+                  className="productImg__slide productImg__footer-slide"
+                  key={index}
+                >
+                  <img className="productImg__footer-img" src={img} />
                 </SwiperSlide>
               ))
             : ''}
         </Swiper>
       </div>
       <EnlargedImageModal
-        currentImage={currentImage}
+        images={images}
+        currentImage={dialogImage}
         flagDialog={flagDialog}
         onclick={() => setFlagDialog(false)}
+        setCurrentImage={setMainSlide}
       />
     </>
   );
