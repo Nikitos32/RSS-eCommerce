858b7bfd075a0a770fad2aceea74754d79cbd3c9
Barvinko <74064627+Barvinko@users.noreply.github.com>
2024-06-03 00:20:08 +0300

    feat: R20-92 Integrate Product Page API Data (#52)
    * feat: add integration of images from API
    
    * feat: add new style for images from API
    
    * feat: add new layout for data from API
    
    * feat: adapt page slider to horizontal images

--- src/components/ProductPage/ProductPage.tsx
+++ src/components/ProductPage/ProductPage.tsx
@@ -1,38 +1,32 @@
 import { ProductSwiper } from './ProductPageSwiper/ProductSwiper';
 import { ButtonSignUp } from '../UI/ButtonSignUp/ButtonSignUp';
+import { useApiGetProduct } from '../../hooks';
+import { useParams } from 'react-router-dom';
+import { ProductAPI } from '../../type/types/productPageType';
 import 'swiper/css';
 import 'swiper/css/free-mode';
 import 'swiper/css/navigation';
 import 'swiper/css/thumbs';
 import './productPage.css';
-import { useApiGetProduct } from '../../hooks';
-import { useParams } from 'react-router-dom';
 import Spinner from '../Spinner';
-
-const images = [
-  'https://content2.rozetka.com.ua/goods/images/big/367689832.jpg',
-  'https://content2.rozetka.com.ua/goods/images/big/367689833.jpg',
-  'https://content1.rozetka.com.ua/goods/images/big/367689834.jpg',
-];
 
 export const ProductPage = () => {
   const { key } = useParams();
   const { loading, product } = useApiGetProduct(key);
+  const productData = product?.data.product.masterData.current as ProductAPI;
+  console.log(productData);
   return (
     <article className="productPage">
       <Spinner isLoading={loading} />
       {!loading && (
         <section className="product">
           <div className="product__component productImg">
-            <ProductSwiper images={images} />
+            <ProductSwiper images={productData.masterVariant.images} />
           </div>
           <div className="product__component productData">
             <div className="productData__title">
-              <div className="font-bold">
-                {product?.data.product.masterData.current.name}
-              </div>
+              <div className="font-bold">{productData.name}</div>
               <div className="font-medium">
-                {product?.data.product.masterData.current.description}
                 <span className="mr-4">$35</span>
                 <span className="text-moonNeutral-500">$50</span>
               </div>
@@ -42,6 +36,10 @@ export const ProductPage = () => {
                 btnContent="ADD TO CARD"
                 customClass="productData__toCard"
               />
+            </div>
+            <div className="productinfo">
+              <h3 className="productinfo__title">Details</h3>
+              {productData.description}
             </div>
           </div>
         </section>
