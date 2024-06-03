8fe4bd5a333ab41ad161e40233a430fcaa2d65fe
Nikita <nikita456nikita@yandex.ru>
2024-06-04 00:14:17 +0300

    feat: add sort functionality

--- src/components/CatalogPage/CatalogPage.tsx
+++ src/components/CatalogPage/CatalogPage.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from 'react';
+import { FormEvent, useContext, useEffect, useState } from 'react';
 import { FilterSection } from '../FilterSection/FilterSection';
 import { ProductPreviewItem } from '../ProductPreviewItem/ProductPreviewItem';
 import { SortSection } from '../SortSection/SortSection';
@@ -19,19 +19,69 @@ interface Products {
 
 export const CatalogPage = () => {
   const [handleLoading] = useContext(IsLoadindContext);
+  const [currentSort, setCurrentSort] = useState<string>();
+  const [currentSearch, setcurrentSearch] = useState<string>();
   const [allProducts, setAllProducts] = useState<Products>();
+
+  const handleCurrentSort = (event: React.ChangeEvent) => {
+    const target = event.target as HTMLInputElement;
+    setCurrentSort(target.value);
+  };
+
+  const handleCurrentSearch = (event: FormEvent) => {
+    const target = event.target as HTMLInputElement;
+    setcurrentSearch(target.value);
+  };
 
   useEffect(() => {
     handleLoading(true);
     const productService = new ProductService();
-    const data: Promise<CTResponse> = productService.getProductsAll();
-    data.then((response) => {
-      handleLoading(false);
-      setAllProducts(
-        ((response.data as CTResponse).data as ProductsData).products
+    if (currentSearch) {
+      /*   const data: Promise<CTResponse> = productService.searchProduct(
+        'en-US',
+        currentSearch
       );
-    });
-  }, []);
+      data.then((response) => {
+        handleLoading(false);
+        setAllProducts(
+          ((response.data as CTResponse).data as ProductsData).products
+        );
+      }); */
+      handleLoading(false);
+    } else {
+      if (currentSort === 'a-z') {
+        const data: Promise<CTResponse> = productService.getProductsSortedByKey(
+          'en-US',
+          'asc'
+        );
+        data.then((response) => {
+          handleLoading(false);
+          setAllProducts(
+            ((response.data as CTResponse).data as ProductsData).products
+          );
+        });
+      } else if (currentSort === 'z-a') {
+        const data: Promise<CTResponse> = productService.getProductsSortedByKey(
+          'en-US',
+          'desc'
+        );
+        data.then((response) => {
+          handleLoading(false);
+          setAllProducts(
+            ((response.data as CTResponse).data as ProductsData).products
+          );
+        });
+      } else {
+        const data: Promise<CTResponse> = productService.getProductsAll();
+        data.then((response) => {
+          handleLoading(false);
+          setAllProducts(
+            ((response.data as CTResponse).data as ProductsData).products
+          );
+        });
+      }
+    }
+  }, [currentSort, currentSearch]);
 
   return (
     <section className="flex">
@@ -39,7 +89,10 @@ export const CatalogPage = () => {
         <FilterSection />
       </div>
       <div className="flex gap-5 flex-col w-full p-5">
-        <SortSection />
+        <SortSection
+          handleCurrentSearch={handleCurrentSearch}
+          handleCurrentSort={handleCurrentSort}
+        />
         <div className="flex flex-col gap-3">
           <h1 className="text-2xl">Products</h1>
           <section className="flex flex-col gap-5 flex-wrap">
--- src/components/SortSection/SortSection.tsx
+++ src/components/SortSection/SortSection.tsx
@@ -1,13 +1,21 @@
 import { SearchInput } from '../UI/SearchInput/SearchInput';
 import { SortSelect } from '../UI/SortSelect/SortSelect';
 
-export const SortSection = () => {
+interface SortSectionProps {
+  handleCurrentSort: (event: React.ChangeEvent) => void;
+  handleCurrentSearch: (event: React.ChangeEvent) => void;
+}
+
+export const SortSection = ({
+  handleCurrentSort,
+  handleCurrentSearch,
+}: SortSectionProps) => {
   return (
     <div className="flex justify-between">
-      <SearchInput />
+      <SearchInput handleCurrentSearch={handleCurrentSearch} />
       <div className="flex gap-2 justify-end items-center">
         <p>Sort by:</p>
-        <SortSelect />
+        <SortSelect handleCurrentSort={handleCurrentSort} />
       </div>
     </div>
   );
