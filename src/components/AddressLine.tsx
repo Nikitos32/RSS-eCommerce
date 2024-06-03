61c3b3f93bc6a05610766b69ebb9d735593068ba
Dima Usov <dmitry.usov@yahoo.com>
2024-06-03 15:01:56 +0300

    feat: add onEdit

--- src/components/AddressLine.tsx
+++ src/components/AddressLine.tsx
@@ -7,31 +7,46 @@ import {
 } from 'react-icons/ci';
 import { AddressForProfile } from '../utils';
 
-type AddressLineProps = { address: AddressForProfile };
-function AddressLine({ address }: AddressLineProps) {
+type AddressLineProps = {
+  address: AddressForProfile;
+  showEdit: boolean;
+  onEdit: (key: string) => void;
+};
+function AddressLine(props: AddressLineProps) {
+  const handleClickEdit = (e: React.MouseEvent<HTMLElement>) => {
+    e.preventDefault();
+    props.onEdit(props.address.id);
+  };
   return (
     <>
       <div className="flex flex-row justify-end gap-1 text-2xl">
-        {address.isDefault && <CiBookmarkCheck title="Default" />}
-        {address.isDefault && address.isShipping && (
+        {props.address.isDefault && <CiBookmarkCheck title="Default" />}
+        {props.address.isDefault && props.address.isShipping && (
           <CiDeliveryTruck title="Shipping Address" />
         )}
-        {address.isDefault && address.isBilling && (
+        {props.address.isDefault && props.address.isBilling && (
           <CiMoneyCheck1 title="Billing Address" />
         )}
-        {!address.isDefault && address.isShipping && (
+        {!props.address.isDefault && props.address.isShipping && (
           <CiDeliveryTruck title="Shipping Address" />
         )}
-        {!address.isDefault && address.isBilling && (
+        {!props.address.isDefault && props.address.isBilling && (
           <CiMoneyCheck1 title="Billing Address" />
         )}
       </div>
-      <p>{address.strAddress}</p>
+      <p>{props.address.strAddress}</p>
       <div className="flex flex-row gap-4 text-2xl">
-        <a href="" title="Edit" className=" hover:text-moonNeutral-600">
-          <CiEdit />
-        </a>
-        {!address.isDefault && (
+        {props.showEdit && (
+          <a
+            href=""
+            onClick={handleClickEdit}
+            title="Edit"
+            className=" hover:text-moonNeutral-600"
+          >
+            <CiEdit />
+          </a>
+        )}
+        {!props.address.isDefault && (
           <a href="" title="Delete" className=" hover:text-moonNeutral-600">
             <CiTrash />
           </a>
