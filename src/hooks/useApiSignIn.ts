f929ece6327a2b56570a45d1d2e132f27d07673d
Dima Usov <dmitry.usov@yahoo.com>
2024-06-09 17:47:52 +0300

    feat: add getActiveCartId

--- src/hooks/useApiSignIn.ts
+++ src/hooks/useApiSignIn.ts
@@ -2,11 +2,13 @@ import { CustomerSignInResult } from '@commercetools/platform-sdk';
 import { CTResponse } from '../ct-client';
 import { CustomerService } from '../services/customer.service';
 import { useAuth } from './useAuth';
+import { ShoppingCartService } from '../services';
 
 export function useApiSignIn(email: string, password: string) {
   const { setCustomerId, setAuthenticated } = useAuth();
 
   const customerService = new CustomerService();
+  const shoppingCartService = new ShoppingCartService();
 
   const signIn = async (): Promise<CTResponse> => {
     const response: CTResponse = await customerService.signIn(email, password);
@@ -16,6 +18,10 @@ export function useApiSignIn(email: string, password: string) {
     setAuthenticated(response.ok);
     if (response.ok) {
       setCustomerId(data.customer.id);
+
+      const responseCart: CTResponse =
+        await shoppingCartService.getActiveCartId(data.customer.id);
+      console.log('Cart', responseCart); // add useShoppingCart
     } else {
       setCustomerId('');
     }
--- src/services/shoppingCart.service.ts
+++ src/services/shoppingCart.service.ts
@@ -1,5 +1,43 @@
+import { ClientResponse } from '@commercetools/platform-sdk';
+import { CTResponse, CTResponseHandler, HttpStatusCode } from '../ct-client';
 import { GraphqlRequest } from '../ct-client/graphql.request';
 
 export class ShoppingCartService {
   graphqlRequest = new GraphqlRequest();
+
+  async getActiveCartId(customerId: string): Promise<CTResponse> {
+    const query = `
+      query {
+        carts(where:"customerId=\\"${customerId}\\" and cartState=\\"Active\\"",
+            sort:"lastModifiedAt Desc") {
+          total
+          results {
+            id
+          }
+        }
+      }`;
+    const variables = {};
+
+    try {
+      const answer = await this.graphqlRequest.make({ query, variables });
+
+      const { total, results } = answer.body.data;
+
+      if (answer.statusCode === HttpStatusCode.OK_200 && total) {
+        return CTResponseHandler.makeSuccess(answer.statusCode, '', results[0]);
+      }
+
+      if (answer.statusCode === HttpStatusCode.OK_200 && !total) {
+        return CTResponseHandler.makeError(
+          HttpStatusCode.NOT_FOUND_404,
+          'No Active Shopping Cart Found',
+          undefined
+        );
+      }
+
+      return CTResponseHandler.handleUnexpectedStatus(answer.statusCode);
+    } catch (error) {
+      return CTResponseHandler.handleCatch(error as ClientResponse);
+    }
+  }
 }
