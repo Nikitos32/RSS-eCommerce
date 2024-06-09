4b5822e6bb961dc7aa51e2d52a25af3523bef948
Dima Usov <dmitry.usov@yahoo.com>
2024-06-09 14:07:21 +0300

    feat: add ShoppingCartService

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,2 +1,3 @@
 export { CustomerService } from './customer.service';
 export { ProductService } from './product.service';
+export { ShoppingCartService } from './shoppingCart.service';
---
+++ src/services/shoppingCart.service.ts
@@ -0,0 +1,5 @@
+import { GraphqlRequest } from '../ct-client/graphql.request';
+
+export class ShoppingCartService {
+  graphqlRequest = new GraphqlRequest();
+}
