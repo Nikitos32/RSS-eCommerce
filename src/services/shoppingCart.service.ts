9281479f48d7a546d4c80d2eccf9e8087e3c213f
Dima Usov <dmitry.usov@yahoo.com>
2024-06-09 18:41:36 +0300

    doc:add  jsdoc

--- src/services/shoppingCart.service.ts
+++ src/services/shoppingCart.service.ts
@@ -4,6 +4,14 @@ import { GraphqlRequest } from '../ct-client/graphql.request';
 
 export class ShoppingCartService {
   graphqlRequest = new GraphqlRequest();
+  /**
+   * @description checks if user with `customerId` has any active shopping carts
+   * and returns the ID of last modified one.
+   *
+   * @param customerId string
+   *
+   * @return CTResponse
+   */
 
   async getActiveCartId(customerId: string): Promise<CTResponse> {
     const query = `
