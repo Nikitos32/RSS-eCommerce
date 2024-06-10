cb489176e132656ba3e93df88268e1b27875f9db
Dima Usov <dmitry.usov@yahoo.com>
2024-06-10 15:05:56 +0300

    refactor: remove ShoppingCartService

--- src/hooks/useApiSignIn.ts
+++ src/hooks/useApiSignIn.ts
@@ -2,13 +2,11 @@ import { CustomerSignInResult } from '@commercetools/platform-sdk';
 import { CTResponse } from '../ct-client';
 import { CustomerService } from '../services/customer.service';
 import { useAuth } from './useAuth';
-import { ShoppingCartService } from '../services';
 
 export function useApiSignIn(email: string, password: string) {
   const { setCustomerId, setAuthenticated } = useAuth();
 
   const customerService = new CustomerService();
-  const shoppingCartService = new ShoppingCartService();
 
   const signIn = async (): Promise<CTResponse> => {
     const response: CTResponse = await customerService.signIn(email, password);
@@ -18,10 +16,7 @@ export function useApiSignIn(email: string, password: string) {
     setAuthenticated(response.ok);
     if (response.ok) {
       setCustomerId(data.customer.id);
-
-      const responseCart: CTResponse =
-        await shoppingCartService.getActiveCartId(data.customer.id);
-      console.log('Cart', responseCart); // add useShoppingCart
+      // loadCart
     } else {
       setCustomerId('');
     }
