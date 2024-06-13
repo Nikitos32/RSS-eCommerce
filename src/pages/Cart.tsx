96bbf9055f05a3ccc1b0c42fbe45ab2c4d8d7183
Barvinko <vlad.barvinko@gmail.com>
2024-06-14 01:53:59 +0300

    feat: add shopping cart product styles to all products

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -6,19 +6,19 @@ function Cart() {
       <h1 className="mb-5 text-center text-2xl font-bold">Cart Items</h1>
       <div className="mx-auto max-w-5xl justify-center px-6 md:flex md:space-x-6 xl:px-0">
         <div className="rounded-lg md:w-2/3">
-          <div className="justify-between mb-6 rounded-lg bg-white p-6 shadow-md sm:flex sm:justify-start">
+          <div className="cartProduct justify-between mb-6 rounded-lg bg-white p-6 shadow-md sm:flex sm:justify-start">
             <img
               src="https://picsum.photos/id/237/200/300"
               alt="product-image"
               className="w-full rounded-lg sm:w-40"
             />
-            <div className="sm:ml-4 sm:flex sm:w-full sm:justify-between">
-              <div className="mt-5 sm:mt-0">
-                <h2 className="text-lg font-bold text-moonBlack">Item 1</h2>
-                <p className="mt-1 text-xs text-moonNeutral-800">Description</p>
+            <div className="cartProduct__containerData sm:ml-4 sm:flex sm:w-full sm:justify-between">
+              <div className="cartProduct__name mt-5 sm:mt-0">
+                <h2 className="text-lg font-bold text-gray-900">Item 1</h2>
+                <p className="mt-1 text-xs text-moonNeutral-700">Description</p>
               </div>
-              <div className="mt-4 flex justify-between sm:space-y-6 sm:mt-0 sm:block sm:space-x-6">
-                <div className="flex items-center border-moonNeutral-300">
+              <div className="cartProduct__priceData mt-4 flex justify-between sm:justify-normal sm:flex-col items-end sm:space-y-6 sm:mt-0 ">
+                <div className="cartProduct__quantity w-min mr-2 flex items-center border-moonNeutral-300">
                   <span className="cursor-pointer rounded-l bg-moonNeutral-300 py-1 px-3.5 duration-100 hover:bg-moonBlack hover:text-blue-50">
                     {' '}
                     -{' '}
@@ -34,7 +34,7 @@ function Cart() {
                     +{' '}
                   </span>
                 </div>
-                <div className="flex items-center space-x-4">
+                <div className="cartProduct__containerPrice flex items-end space-x-4">
                   <PriceProduct
                     initialPrice={{ centAmount: 25900, fractionDigits: 2 }}
                   />
@@ -44,7 +44,7 @@ function Cart() {
                     viewBox="0 0 24 24"
                     strokeWidth="1.5"
                     stroke="currentColor"
-                    className="h-5 w-5 cursor-pointer duration-150 hover:text-red-500"
+                    className="h-5 w-5 mb-1.5 cursor-pointer duration-150 hover:text-red-500"
                   >
                     <path
                       strokeLinecap="round"
