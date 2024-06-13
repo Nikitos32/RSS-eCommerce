aaa21ca70194b620afb609a6884d8408946bceab
Dima Usov <dmitry.usov@yahoo.com>
2024-06-13 20:28:58 +0300

    feat: add updateShoppingCart

--- src/context/ShoppingCartProvider.tsx
+++ src/context/ShoppingCartProvider.tsx
@@ -5,7 +5,13 @@ import { CTResponse } from '../ct-client';
 import {
   CustomerSignInResult,
   GraphQLResponse,
+  Image,
+  LineItem,
 } from '@commercetools/platform-sdk';
+import {
+  ProductInShoppingCart,
+  ShoppingCart,
+} from '../services/shoppingCart.service';
 
 type ShoppingCartProviderProps = {
   children?: ReactNode;
@@ -41,6 +47,8 @@ export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
   );
   const shoppingCartService = new ShoppingCartService();
 
+  const [shoppingCart, setShoppingCart] = useState<ShoppingCart>();
+
   const [activeCartId, setActiveCartId] = useLocalStorage('apiCartId', '');
   const [activeCartVersion, setActiveCartVersion] = useState(0);
 
@@ -52,6 +60,56 @@ export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
       cartItems.find((item) => item.productId === productId)?.quantity || 0
     );
   }
+
+  function updateShoppingCart(response: GraphQLResponse) {
+    const {
+      id = '',
+      version = 0,
+      totalLineItemQuantity = 0,
+      totalPrice = null,
+      lineItems = [],
+    } = response.data.updateCart;
+
+    const products: ProductInShoppingCart = {};
+
+    if (lineItems) {
+      lineItems.forEach((item: LineItem) => {
+        const {
+          productId = '',
+          name = '',
+          quantity = 0,
+          variant,
+          price: { value: price },
+        } = item;
+
+        const productName = name as string;
+
+        const firstImage = variant?.images ? variant?.images[0] : ({} as Image);
+
+        const { url: imageUrl = '', label: imageLabel = productName } =
+          firstImage;
+
+        products[productId] = {
+          name: productName,
+          quantity,
+          price,
+          imageUrl,
+          imageLabel,
+        };
+      });
+    }
+    const shoppingCartUpdate: ShoppingCart = {
+      id,
+      version,
+      totalPrice,
+      totalLineItemQuantity,
+      products,
+    };
+    console.log('Update', shoppingCartUpdate);
+
+    setShoppingCart(shoppingCartUpdate);
+  }
+
   async function increaseProductQuantity(
     productId: string
   ): Promise<CTResponse> {
@@ -65,7 +123,9 @@ export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
       return answer;
     }
     const response = answer.data as GraphQLResponse;
+    updateShoppingCart(response);
     setCartVersion(response.data.updateCart.version);
+    console.log('shoppingCart', shoppingCart);
 
     setCartItems((currentItems) => {
       if (
@@ -111,10 +171,11 @@ export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
     });
   }
 
-  const total = cartItems.reduce(
-    (quantity, item) => item.quantity + quantity,
-    0
-  );
+  const total = shoppingCart?.totalLineItemQuantity || 0;
+  // cartItems.reduce(
+  //   (quantity, item) => item.quantity + quantity,
+  //   0
+  // );
 
   function setCartId(activeCartId: string) {
     setActiveCartId(activeCartId);
