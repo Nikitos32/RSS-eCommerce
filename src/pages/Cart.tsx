f7cca0d91d39e9d20bf08043024149133bffc881
Barvinko <vlad.barvinko@gmail.com>
2024-06-14 01:24:03 +0300

    feat: integrate component PriceProduct to Cart

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,3 +1,5 @@
+import { PriceProduct } from '../components/PriceProduct/PriceProduct';
+
 function Cart() {
   return (
     <section className="bg-moonNeutral-200 pt-5">
@@ -33,7 +35,9 @@ function Cart() {
                   </span>
                 </div>
                 <div className="flex items-center space-x-4">
-                  <p className="text-sm">259.00</p>
+                  <PriceProduct
+                    initialPrice={{ centAmount: 25900, fractionDigits: 2 }}
+                  />
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
@@ -52,25 +56,25 @@ function Cart() {
               </div>
             </div>
           </div>
-          <div className="justify-between mb-6 rounded-lg bg-white p-6 shadow-md sm:flex sm:justify-start">
+          <div className="cartProduct justify-between mb-6 rounded-lg bg-white p-6 shadow-md sm:flex sm:justify-start">
             <img
               src="https://picsum.photos/id/40/100/100"
               alt="product-image"
               className="w-full rounded-lg sm:w-40"
             />
-            <div className="sm:ml-4 sm:flex sm:w-full sm:justify-between">
-              <div className="mt-5 sm:mt-0">
+            <div className="cartProduct__containerData sm:ml-4 sm:flex sm:w-full sm:justify-between">
+              <div className="cartProduct__name mt-5 sm:mt-0">
                 <h2 className="text-lg font-bold text-gray-900">Item 2</h2>
                 <p className="mt-1 text-xs text-moonNeutral-700">Description</p>
               </div>
-              <div className="mt-4 flex justify-between im sm:space-y-6 sm:mt-0 sm:block sm:space-x-6">
-                <div className="flex items-center border-moonNeutral-300">
+              <div className="cartProduct__priceData mt-4 flex justify-between sm:justify-normal sm:flex-col items-end sm:space-y-6 sm:mt-0 ">
+                <div className="cartProduct__quantity w-min mr-2 flex items-center border-moonNeutral-300">
                   <span className="cursor-pointer rounded-l bg-moonNeutral-300 py-1 px-3.5 duration-100 hover:bg-moonBlack hover:text-blue-50">
                     {' '}
                     -{' '}
                   </span>
                   <input
-                    className="h-8 w-8 border bg-white text-center text-xs outline-none"
+                    className="h-8 w-8 border bg-white text-center text-xs outline-none [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
                     type="number"
                     value="2"
                     min="1"
@@ -80,15 +84,19 @@ function Cart() {
                     +{' '}
                   </span>
                 </div>
-                <div className="flex items-center space-x-4">
-                  <p className="text-sm">259.00</p>
+                <div className="cartProduct__containerPrice flex items-end space-x-4">
+                  <PriceProduct
+                    initialPrice={{ centAmount: 26000, fractionDigits: 2 }}
+                    discountPrice={{ centAmount: 13000, fractionDigits: 2 }}
+                    discountValue={5000}
+                  />
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
                     viewBox="0 0 24 24"
                     strokeWidth="1.5"
                     stroke="currentColor"
-                    className="h-5 w-5 cursor-pointer duration-150 hover:text-red-500"
+                    className="h-5 w-5 mb-1.5 cursor-pointer duration-150 hover:text-red-500"
                   >
                     <path
                       strokeLinecap="round"
