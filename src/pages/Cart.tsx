2c50321d45f9e6c724d45afb3427d87ffc0c4270
Dima Usov <dmitry.usov@yahoo.com>
2024-06-15 00:22:30 +0300

    feat: add input for promo code

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,8 +1,17 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useShoppingCart } from '../hooks';
 import CartItem from '../components/CartItem';
+import UserInputString from '../components/UserInputString';
 
 function Cart() {
+  const inputStringInitState = {
+    value: '',
+    valid: true,
+    visibleClue: false,
+    focus: false,
+    readonly: false,
+  };
+
   const { refreshShoppingCart, getShoppingCartProducts } = useShoppingCart();
 
   useEffect(() => {
@@ -10,6 +19,8 @@ function Cart() {
   }, []); //  will only run on load
 
   const products = getShoppingCartProducts();
+
+  const [promoCode, setPromoCode] = useState(inputStringInitState);
 
   return (
     <section className="bg-moonNeutral-200 pt-5">
@@ -22,6 +33,25 @@ function Cart() {
         </div>
         {/*-- Sub total */}
         <div className="mt-6 h-full rounded-lg border bg-white p-6 shadow-md md:mt-0 md:w-1/3">
+          <div className="mb-1 flex justify-center gap-1 content-end">
+            <div className="">
+              <UserInputString
+                type="text"
+                label="Promo Code"
+                placeHolder="Enter Code"
+                autocomplete="off"
+                state={promoCode}
+                setState={setPromoCode}
+              />
+            </div>
+            <button
+              className="bg-moonBlack text-moonNeutral-100 rounded-lg px-4 py-2 hover:bg-moonNeutral-600 focus:outline-none focus:shadow-outline disabled:bg-moonNeutral-500 disabled:cursor-not-allowed self-end"
+              type="button"
+              disabled={!promoCode.value}
+            >
+              Redeem
+            </button>
+          </div>
           <div className="mb-2 flex justify-between">
             <p className="text-moonNeutral-700">Subtotal</p>
             <p className="text-moonNeutral-700">$129.99</p>
