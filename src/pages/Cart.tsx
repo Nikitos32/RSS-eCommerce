1bc5d860cae0d859694e1175a1c4dcd162d3e0b1
Dima Usov <dmitry.usov@yahoo.com>
2024-06-14 14:43:10 +0300

    feat: add CartItem

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { PriceProduct } from '../components/PriceProduct/PriceProduct';
 import { useShoppingCart } from '../hooks';
+import CartItem from '../components/CartItem';
 
 function Cart() {
   const { refreshShoppingCart } = useShoppingCart();
@@ -13,56 +14,7 @@ function Cart() {
       <h1 className="mb-5 text-center text-2xl font-bold">Cart Items</h1>
       <div className="mx-auto max-w-5xl justify-center px-6 md:flex md:space-x-6 xl:px-0">
         <div className="rounded-lg md:w-2/3">
-          <div className="cartProduct justify-between mb-6 rounded-lg bg-white p-6 shadow-md sm:flex sm:justify-start">
-            <img
-              src="https://picsum.photos/id/237/200/300"
-              alt="product-image"
-              className="w-full rounded-lg sm:w-40"
-            />
-            <div className="cartProduct__containerData sm:ml-4 sm:flex sm:w-full sm:justify-between">
-              <div className="cartProduct__name mt-5 sm:mt-0">
-                <h2 className="text-lg font-bold text-gray-900">Item 1</h2>
-                <p className="mt-1 text-xs text-moonNeutral-700">Description</p>
-              </div>
-              <div className="cartProduct__priceData mt-4 flex justify-between sm:justify-normal sm:flex-col items-end sm:space-y-6 sm:mt-0 ">
-                <div className="cartProduct__quantity w-min mr-2 flex items-center border-moonNeutral-300">
-                  <span className="cursor-pointer rounded-l bg-moonNeutral-300 py-1 px-3.5 duration-100 hover:bg-moonBlack hover:text-blue-50">
-                    {' '}
-                    -{' '}
-                  </span>
-                  <input
-                    className="h-8 w-8 border bg-white text-center text-xs outline-none [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
-                    type="number"
-                    value="2"
-                    min="1"
-                  />
-                  <span className="cursor-pointer rounded-r bg-moonNeutral-300 py-1 px-3 duration-100 hover:bg-moonBlack hover:text-blue-50">
-                    {' '}
-                    +{' '}
-                  </span>
-                </div>
-                <div className="cartProduct__containerPrice flex items-end space-x-4">
-                  <PriceProduct
-                    initialPrice={{ centAmount: 25900, fractionDigits: 2 }}
-                  />
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="currentColor"
-                    className="h-5 w-5 mb-1.5 cursor-pointer duration-150 hover:text-red-500"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M6 18L18 6M6 6l12 12"
-                    />
-                  </svg>
-                </div>
-              </div>
-            </div>
-          </div>
+          <CartItem />
           <div className="cartProduct justify-between mb-6 rounded-lg bg-white p-6 shadow-md sm:flex sm:justify-start">
             <img
               src="https://picsum.photos/id/40/100/100"
