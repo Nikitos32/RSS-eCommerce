f59be2e801c59a9162ced96ec71ceffe70168934
Barvinko <vlad.barvinko@gmail.com>
2024-06-15 19:29:56 +0300

    feat: add styles to text CartEmpty

--- src/components/CartEmpty/CartEmpty.tsx
+++ src/components/CartEmpty/CartEmpty.tsx
@@ -2,8 +2,10 @@ export const CartEmpty = () => {
   return (
     <article className="containerMain flex flex-col justify-center items-center">
       <img src="../../../public\svg\CartEmpty.svg" alt="Cart" />
-      <h1>Cart is empty</h1>
-      <h3>{"But it's never too late to fix it :)"}</h3>
+      <h1 className="text-xl font-semibold">Cart is empty</h1>
+      <h3 className="text-moonNeutral-600">
+        {"But it's never too late to fix it :)"}
+      </h3>
     </article>
   );
 };
