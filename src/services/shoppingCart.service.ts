98f4d5c291f660a8f90d53c3eba6f8a325c3f62d
Dima Usov <dmitry.usov@yahoo.com>
2024-06-15 13:55:35 +0300

    refactor: remove unused getActiveCartId

--- src/services/shoppingCart.service.ts
+++ src/services/shoppingCart.service.ts
@@ -1,6 +1,6 @@
 import { CartDraft, Price, TypedMoney } from '@commercetools/platform-sdk';
 
-import { CTResponse, CTResponseHandler, HttpStatusCode } from '../ct-client';
+import { CTResponse, CTResponseHandler } from '../ct-client';
 import { GraphqlRequest } from '../ct-client/graphql.request';
 
 const {
@@ -68,55 +68,10 @@ export interface ShoppingCart {
   totalLineItemQuantity: number;
   totalPrice: TypedMoney;
   products: ShoppingCartItem;
-
 }
 
 export class ShoppingCartService {
   graphqlRequest = new GraphqlRequest();
-  /**
-   * @description checks if user with `customerId` has any active shopping carts
-   * and returns the ID of last modified one.
-   *
-   * @param customerId string
-   *
-   * @return CTResponse
-   */
-
-  async getActiveCartId(customerId: string): Promise<CTResponse> {
-    const query = `
-      query {
-        carts(where:"customerId=\\"${customerId}\\" and cartState=\\"Active\\"",
-            sort:"lastModifiedAt Desc") {
-          total
-          results {
-            id
-          }
-        }
-      }`;
-    const variables = {};
-
-    try {
-      const answer = await this.graphqlRequest.make({ query, variables });
-
-      const { total, results } = answer.body.data;
-
-      if (answer.statusCode === HttpStatusCode.OK_200 && total) {
-        return CTResponseHandler.makeSuccess(answer.statusCode, '', results[0]);
-      }
-
-      if (answer.statusCode === HttpStatusCode.OK_200 && !total) {
-        return CTResponseHandler.makeError(
-          HttpStatusCode.NOT_FOUND_404,
-          'No Active Shopping Cart Found',
-          undefined
-        );
-      }
-
-      return CTResponseHandler.handleUnexpectedStatus(answer.statusCode);
-    } catch (error) {
-      return CTResponseHandler.handleCatch(error);
-    }
-  }
 
   private async createCart(cartDraft: CartDraft): Promise<CTResponse> {
     const query = `
