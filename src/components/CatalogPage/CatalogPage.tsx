dc00d8bf6918f2e6504bdc00c34e078451b81996
Nikita <114023790+Nikitos32@users.noreply.github.com>
2024-06-17 17:55:38 +0300

    fix: R20-31 fix catalog page (#95)
    * fix: fix pattern for login and registration
    
    * feat: add picture
    
    * refactor: add new text
    
    * fix: delete all comments
    
    * fix: fix loader on catalog page and amount of uploaded products
    
    * refactor: delete unnecessary files
    
    * feat: add refresh cart and return comments
    
    * feat: returned comments
    
    * fix: set current limit 13 and returned spinner component

--- src/components/CatalogPage/CatalogPage.tsx
+++ src/components/CatalogPage/CatalogPage.tsx
@@ -14,6 +14,7 @@ import { animateScroll as scroll } from 'react-scroll';
 import { Oval } from 'react-loader-spinner';
 import './catalogPage.css';
 import { useShoppingCart } from '../../hooks';
+import Spinner from '../Spinner';
 
 interface ProductProjectionResponse {
   productProjectionSearch: ProductProjectionPagedQueryResponse;
