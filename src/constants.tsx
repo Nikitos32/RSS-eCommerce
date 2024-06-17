eeba94d248cf5b5818bd390901dd976d891035a4
Nikita <nikita456nikita@yandex.ru>
2024-06-17 12:21:43 +0300

    fix: fix pattern for login and registration

--- src/constants.tsx
+++ src/constants.tsx
@@ -21,7 +21,7 @@ export enum Links {
 export const MIN_PASSWORD_LENGTH: number = 8;
 
 export const REGEX_FOR_EMAIL_INPUT: string =
-  '[a-zA-Z0-9]+@(?:[a-zA-Z0-9]+\\.)+[A-Za-z]{2,4}';
+  '[a-zA-Z0-9.]+@(?:[a-zA-Z0-9]+\\.)+[A-Za-z]{2,4}';
 export const REGEX_FOR_PASSWORD_INPUT: string =
   '^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*]).*$';
 
