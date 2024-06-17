e1d67f640eee5842b47eb6076ad5529427d70e8d
Barvinko <vlad.barvinko@gmail.com>
2024-06-17 15:47:36 +0300

    fix: fix url to CartEmpty.svg for netlify

--- src/components/CartEmpty/CartEmpty.tsx
+++ src/components/CartEmpty/CartEmpty.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 export const CartEmpty = () => {
   return (
     <article className="containerMain flex flex-col justify-center items-center">
-      <img src="../../../public\svg\CartEmpty.svg" alt="Cart" />
+      <img src="svg\CartEmpty.svg" alt="Cart" />
       <h1 className="text-xl font-semibold">Cart is empty</h1>
       <h3 className="text-moonNeutral-600">
         {"But it's never too late to fix it :)"}
